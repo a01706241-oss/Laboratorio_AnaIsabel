370edbd8725e938439b1a16194ce6998f2950edf
Ana Isabel García Mojica <anaisabelgarciamojica@MacBook-Air-de-Ana-120.local>
2025-10-31 16:39:57 -0600

    Commit1

---
+++ Programa.c
@@ -0,0 +1,19 @@
+{\rtf1\ansi\ansicpg1252\cocoartf2820
+\cocoatextscaling0\cocoaplatform0{\fonttbl\f0\froman\fcharset0 TimesNewRomanPSMT;\f1\fswiss\fcharset0 ArialMT;}
+{\colortbl;\red255\green255\blue255;\red0\green0\blue0;}
+{\*\expandedcolortbl;;\csgray\c0;}
+\margl1440\margr1440\vieww11520\viewh8400\viewkind0
+\pard\tx560\tx1120\tx1680\tx2240\tx2800\tx3360\tx3920\tx4480\tx5040\tx5600\tx6160\tx6720\pardirnatural\partightenfactor0
+
+\f0\fs24 \cf2 #include <stdio.h>\
+#include <stdlib.h>\
+#include <math.h>\
+int main()\{\
+int length;\
+scanf("%i", &length);\
+int office = pow(2,length);\
+printf("%i",office);\
+return 0;\
+\}\
+4)
+\f1  }

e49a2db582747c2df7df3f6d1e2156606e045678
Ana Isabel García Mojica <anaisabelgarciamojica@MacBook-Air-de-Ana-120.local>
2025-11-02 13:30:20 -0600

    Commit1

---
+++ Operacion.c
@@ -0,0 +1,23 @@
+{\rtf1\ansi\ansicpg1252\cocoartf2820
+\cocoatextscaling0\cocoaplatform0{\fonttbl\f0\froman\fcharset0 TimesNewRomanPSMT;\f1\fswiss\fcharset0 Helvetica;}
+{\colortbl;\red255\green255\blue255;\red0\green0\blue0;}
+{\*\expandedcolortbl;;\csgray\c0;}
+\margl1440\margr1440\vieww11520\viewh8400\viewkind0
+\pard\tx560\tx1120\tx1680\tx2240\tx2800\tx3360\tx3920\tx4480\tx5040\tx5600\tx6160\tx6720\pardirnatural\partightenfactor0
+
+\f0\fs24 \cf2 #include <stdio.h>\
+#include <stdlib.h>\
+#include <math.h>
+\f1\fs22\fsmilli11040 12\
+
+\f0\fs24 int main()\{\
+int num1 = 12;\
+int num2 = 3;\
+int op1 = suma(num1,num2);\
+printf("%i",op1);\
+int op2 = resta(num1,num2);\
+printf("%i",op2);\
+int op3 = multi(num1,num2);\
+printf("%i",op3);\
+return 0;\
+\}}
